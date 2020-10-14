c1383089f69aae5ee5f07f26acf99abb5e84ae1b
sales <sales@angularadvisors.com>
2020-10-14 08:45:48 -0600

    initial code commit

---
+++ OrchardSkills.OIDC.BlazorWASM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrchardSkills.OIDC.BlazorWASM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
